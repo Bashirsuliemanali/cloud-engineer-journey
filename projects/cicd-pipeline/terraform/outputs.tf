8c3cf107508f200fcd76abac359f14b7b2b0f4b3
Bashirsuliemanali <your-github-email@icloud.com>
2026-04-13 01:36:59 +0100

    trigger ci/cd pipeline - update app message

--- projects/cicd-pipeline/terraform/outputs.tf
+++ projects/cicd-pipeline/terraform/outputs.tf
@@ -7,3 +7,8 @@ output "ecr_repository_name" {
   description = "ECR repository name"
   value       = aws_ecr_repository.app.name
 }
+
+output "github_actions_role_arn" {
+  description = "GitHub Actions IAM role ARN"
+  value       = aws_iam_role.github_actions.arn 
+}
